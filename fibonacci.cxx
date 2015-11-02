6532ae6bf159a3035c779d2478ea61c8ee2636a4
Philipp Wachenfeld <PhilWac@linux-ywbc.site>
2015-11-02 20:20:58 +0100

    Homework 1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+// Funktionen
+int fibo(int z){
+    if(z == 1)
+         return 0;
+    if(z==1 || z==2)
+        return 1;
+    else
+        return fibo(z-1)+fibo(z-2);
+}
+
+
+
+// Hauptprogramm
+int main() {
+
+    for(int i=1; i<=1; i++)
+        cout << "Fibo: " << fibo(11) << endl;
+
+  
+    return 0;
+}
